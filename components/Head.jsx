698b73f654d32922f827585022ab8f3584922159
Sylène Manusset <sylene.manusset@gmail.com>
2019-12-05 22:18:32 +0100

    installation of reactsrap + creating a first Head component, which is called a Bootswatch theme

---
+++ components/Head.jsx
@@ -0,0 +1,14 @@
+import Head from "next/head";
+
+const WithHead = () => {
+  return (
+    <Head>
+      <title>Ma petite création</title>
+      <link
+        rel="stylesheet"
+        href="https://bootswatch.com/4/minty/bootstrap.min.css"
+      />
+    </Head>
+  );
+};
+export default WithHead;
