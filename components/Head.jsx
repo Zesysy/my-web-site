421e9c8872067f80aafee46c6cf7d5dc37eb3d90
Sylène Manusset <sylene.manusset@gmail.com>
2019-12-06 18:31:33 +0100

    [WIP] add Footer with a display condition > change of Footer content

--- components/Head.jsx
+++ components/Head.jsx
@@ -2,13 +2,26 @@ import Head from "next/head";
 
 const WithHead = () => {
   return (
-    <Head>
-      <title>Ma petite création</title>
-      <link
-        rel="stylesheet"
-        href="https://bootswatch.com/4/minty/bootstrap.min.css"
-      />
-    </Head>
+    <>
+      <Head>
+        <title>Ma petite création</title>
+        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+        <link
+          rel="stylesheet"
+          href="https://bootswatch.com/4/minty/bootstrap.min.css"
+        />
+      </Head>
+      <style jsx>
+        {`
+          body {
+            min-height: 100%;
+            margin: 0;
+            padding: 0;
+            position: relative;
+          }
+        `}
+      </style>
+    </>
   );
 };
 export default WithHead;
