5b2e5cc871c969b75c1530f595341344dc8f5522
sylene <sylene.manusset@gmail.com>
2020-11-18 15:42:02 +0100

    Add google analytics

--- components/structure/Head.jsx
+++ components/structure/Head.jsx
@@ -1,9 +1,36 @@
 import Head from "next/head";
 
+import { GA_TRACKING_ID } from "../../lib/gtag";
+
 const WithHead = () => {
+  // Check if in production
+  const isProduction = process.env.NODE_ENV === "production";
+
   return (
     <>
       <Head>
+        {isProduction && (
+          <>
+            {/* Global Site Tag (gtag.js) - Google Analytics */}
+            <script
+              async
+              src={`https://www.googletagmanager.com/gtag/js?id=${GA_TRACKING_ID}`}
+            />
+            <script
+              dangerouslySetInnerHTML={{
+                __html: `
+                    window.dataLayer = window.dataLayer || [];
+                    function gtag(){dataLayer.push(arguments);}
+                    gtag('js', new Date());
+
+                    gtag('config', '${GA_TRACKING_ID}', {
+                      page_path: window.location.pathname,
+                    });
+                  `,
+              }}
+            />
+          </>
+        )}
         <title>
           Ma petite création - Portfolio d'un développeur javascript et java -
           Sylène Manusset
